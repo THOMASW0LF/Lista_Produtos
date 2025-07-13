41c20b8683735a4226d8ce85a79e48969120350f
THOMASW0LF <thomasgwolf1@gmail.com>
2025-07-12 22:43:36 -0300

    cria rota

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\CategoriaController;
+
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::resource('produtos', ProdutoController::class);
+Route::resource('categorias', CategoriaController::class);
