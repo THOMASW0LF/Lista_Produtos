c7f5d2a0c0df666beb9c18afdf8e58dd8c452503
THOMASW0LF <thomasgwolf1@gmail.com>
2025-07-12 22:42:42 -0300

    cria controlers

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produto;
+use App\Models\Categoria;
+
+class ProdutoController extends Controller
+{
+    public function index()
+    {
+        $produtos = Produto::with('categoria')->get();
+        return view('produtos.index', compact('produtos'));
+    }
+
+    public function create()
+    {
+        $categorias = Categoria::all();
+        return view('produtos.create', compact('categorias'));
+    }
+
+    public function store(Request $request)
+    {
+        Produto::create($request->all());
+        return redirect()->route('produtos.index');
+    }
+
+    public function edit(Produto $produto)
+    {
+        $categorias = Categoria::all();
+        return view('produtos.edit', compact('produto', 'categorias'));
+    }
+
+    public function update(Request $request, Produto $produto)
+    {
+        $produto->update($request->all());
+        return redirect()->route('produtos.index');
+    }
+
+    public function destroy(Produto $produto)
+    {
+        $produto->delete();
+        return redirect()->route('produtos.index');
+    }
+
+   
+}
